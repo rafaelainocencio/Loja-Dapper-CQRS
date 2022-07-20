b17981798e0e3f6c1cfe904a95b30497d320268b
rafaela montalvao <rafaelainocencioo@gmail.com>
2022-07-20 13:26:45 -0300

    Entities

---
+++ Domain/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja.Domain
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; protected set; }
+    }
+}
---
+++ Domain/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja.Domain
+{
+    public class Categoria : BaseEntity
+    {
+
+    }
+}
